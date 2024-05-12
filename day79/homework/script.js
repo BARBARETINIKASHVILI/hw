d1343d6fec690b27099aa50796222cef6f68c5ed
BARBARETINIKASHVILI <tinikashvilibarbare2@gmail.com>
2024-05-12 22:24:17 +0400

    commit

---
+++ day79/homework/script.js
@@ -0,0 +1,12 @@
+//1
+//1. Transforming Data: Given an array of objects representing people with their ages, use the map function to create a new array containing only the names of these people.
+
+//გვიჩვენებს რუკის ფუნქციის გამოყენებას ხალხის მასივის თითოეული ობიექტიდან სახელის საკუთრების ამოსაღებად და ახალ მასივში სახელად სახელების შესანახად
+//const people = [{name: 'mari', age:20, }, 
+//                {name: 'ana', age:28,}, 
+//                {name: 'aleko', age:30 }];
+//const names = people.map(person => person.name); //აქ რუკის ფუნქცია გამოიძახება ხალხის მასივზე.=აბრუნებს მისი სახელის თვისების მნიშვნელობას.
+//console.log(names); 
+
+
+
