63f9fbabe8420986e6166cd8949d2c8e4fc5ac9a
BARBARETINIKASHVILI <tinikashvilibarbare2@gmail.com>
2024-06-03 20:29:16 +0400

    commit

---
+++ day87/homework/script.js
@@ -0,0 +1,33 @@
+// use strict - მკაცრი რეჟიმი 
+
+// charAt()
+
+// const name = "barbare"
+// console.log(name.charAt(4)) // ამოიღებს მეოთხე ინდექსზე მდგომს ანუ a-ს
+
+// trim()
+
+// const name = "     barbare   "
+// console.log(name.trim()) // მოაშორებს სფეისებს აქეთ იქიდან
+
+// isArray()
+// function myFunction() {
+//     var fruits = ["ბარბარე", "ლიზი",];
+//     me = Array.isArray(fruits);
+//   }
+
+//   console.log(myFunction)
+
+
+//forEach()
+
+// ანაწილებს სიას მაგალითად [1, 3, 5,] 1
+// განაწილდება                         3
+//                                     5
+
+//Array map()
+
+//ქმნის ახალ მასივს
+
+// www.w3schools.com/js/js_es5.asp
+// developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map  
